c98cfbf18f009991b33c9a43c08930ff1f93a330
Mouazzama <mouazzamabinteumer.ep19102044@gmail.com>
2022-11-21 22:53:31 -0800

    files

---
+++ next/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <h2 >
+      YO
+    </h2>
+  )
+}
